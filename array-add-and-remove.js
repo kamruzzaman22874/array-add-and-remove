f21c7097f850269dbd8818049a0caa2c586973f0
kamruzzaman22874 <joybd.eee@gmail.com>
2023-03-09 13:00:40 +0600

    This is second commit

--- array-add-and-remove.js
+++ array-add-and-remove.js
@@ -9,9 +9,11 @@ const newProduct = {name: 'keyboard', price: 1000, model: 'Micropack'}
 
 // Add new item with totally make a new array 
 const newArray = [...products, newProduct]
-// console.log(newArray);
+console.log(newArray);
 
 // ekhon ami uporer array theke laptop namer item take remove kore notun arekti array te rakhbo 
 
 const remainingProduct = products.filter(product => product.name !== 'laptop');
 console.log(remainingProduct);
+
+// Can you apply another rules 
